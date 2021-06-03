4fd90a6d2f16a9ccb0301a2671ca345e1ff74686
alkautsarm <alkautsarm@gmail.com>
2021-06-03 23:25:56 +0700

    feat: add predict cluster function

--- routers/user.py
+++ routers/user.py
@@ -13,6 +13,15 @@ from config import settings
 from db.mongodb import mongo_engine
 from models.user import User, UserUpdate
 
+# Packages needed for ML
+from models.genre import Genre
+from choice.education import EducationChoice
+import sklearn.preprocessing as prep
+import numpy as np
+import tensorflow as tf
+import joblib
+import umap
+
 router = APIRouter(
     tags=["user"],
     prefix="/user"
@@ -96,3 +105,103 @@ async def delete(id: ObjectId, engine: AIOEngine = Depends(mongo_engine)):
         raise HTTPException(404)
     await engine.delete(user)
     return user
+
+@router.patch("/predict/{id}")
+async def predict_user_cluser(id: ObjectId, engine: AIOEngine = Depends(mongo_engine)):
+    # Preprocess user data section
+
+    # Get user by Id
+    user = await engine.find_one(User, User.id == id)
+    if user is None:
+        raise HTTPException(404)
+    
+    # Check user.age == isNotNull ? continue : raise error (age need to be filled first)
+    if user.age is None:
+        return {
+            'isPredicted': False,
+            'message': 'Data umur kosong. Silahkan isi umur terlebih dahulu.'
+        }
+
+    # Check user.education == isNotNull ? continue : raise error (education need to be filled first)
+    if user.education is None:
+        return {
+            'isPredicted': False,
+            'message': 'Data pendidikan kosong. Silahkan isi pendidikan terlebih dahulu.'
+        }
+
+    # Check user.genre_preferences == isNotNull ? continue : raise error (genre preference need to be filled first)
+    if user.genre_preferences is None:
+        return {
+            'isPredicted': False,
+            'message': 'Data preferensi genre buku kosong. Silahkan isi preferensi genre buku terlebih dahulu.'
+        }
+
+    # Change age to np array
+    user_age = np.array([[user.age]])
+
+    # One hot encode user.education using list education as encoder categories
+    user_edu_encoded = encode_education(user.education)
+    
+    # One hot encode user.genre_preferences using list genre as encoder categories
+    genre_list = await engine.find(Genre)
+    user_genre_encoded = encode_genre(user.genre_preferences, genre_list)
+    
+    # Predict user cluster section
+    # Pass Data to TF AutoEncoder Model
+    combined_user_data_np = np.concatenate((user_age[0], user_edu_encoded[0], user_genre_encoded[0]))
+    combined_user_data_tensor = tf.convert_to_tensor([combined_user_data_np])
+
+    ae_model = tf.keras.models.load_model('ml-assets/habitech_autoencoder')
+    ae_result = ae_model.predict(combined_user_data_tensor)
+
+    # Pass embedded data to UMAP
+    # Currently, by load saved umap model will create error (Incompatible bytecode version)
+    # umap_model = joblib.load('ml-assets/habitech_umap.sav')
+    umap_model = umap.UMAP(n_neighbors=20, n_components=33, metric='euclidean', min_dist=0, random_state=0)
+    umap_result = umap_model.fit_transform(ae_result)
+
+    # Pass Data that has been processed by UMAP to GMM
+    gmm_model = joblib.load('ml-assets/habitech_gmm.sav')
+    gmm_result = gmm_model.predict_proba(umap_result)
+
+    # Get cluster
+    cluster_predicted = gmm_result.argmax(1)
+
+    # Update user.reading_cluster
+    setattr(user, 'reading_cluster', cluster_predicted[0])
+    await engine.save(user)
+
+    return {
+        'isPredicted': True,
+        'userData': user
+    }
+
+def encode_education(user_edu):
+    edu_list = [edu.value for edu in EducationChoice]
+
+    enc_edu = prep.OneHotEncoder(categories=[edu_list])
+
+    # Need to be in 2D Array (currently in String)
+    enc_edu_fit = enc_edu.fit_transform([[user_edu]])
+
+    return enc_edu_fit.toarray()
+
+def encode_genre(user_genre_pref, genre_list_db):
+    user_genre_pref = [user_genre.name for user_genre in user_genre_pref]
+    genre_list = [genre.name for genre in genre_list_db]
+    
+    # Encode genre one by one
+    encoded_genre_pref = []
+    for genre in user_genre_pref:
+        enc_genre = prep.OneHotEncoder(categories=[genre_list])
+
+        # Need to be in 2D Array (currently in String)
+        enc_genre_fit = enc_genre.fit_transform([[genre]])
+        encoded_genre_pref.append(enc_genre_fit.toarray())
+    
+    # Combine all encoded genre
+    combined_genre = encoded_genre_pref[0]
+    for encoded_genre in encoded_genre_pref[1:]:
+        combined_genre = np.add(combined_genre, encoded_genre)
+
+    return combined_genre
